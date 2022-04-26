835708d844337e5acb2d63a05b7763d29418c36e
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-04-26 13:40:40 +0000

    updated find_modules

--- scripts/find_modules.py
+++ scripts/find_modules.py
@@ -1,6 +1,7 @@
 import re
 import os
 import logging
+import sys
 
 from ast import literal_eval
 from glob import glob
@@ -20,7 +21,7 @@ def parse_file(path):
         data = read_file(path)
         parsed_data = []
         if fileext == '.py':
-            parsed_data = [x for x in data if not x.startswith("#")]
+            parsed_data = [x for x in data.split("\n") if not x.startswith("#")]
 
         PARSED_FILES[path] = parsed_data
     return PARSED_FILES.get(path)
@@ -40,24 +41,70 @@ def find_modules(root_directory=ROOT_PATH, include_core=True):
             module_name = os.path.basename(os.path.dirname(manifest_path))
             yield module_name, manifest_path
 
-def parse_deps(manifest_path):
-    manifest = literal_eval(read_file(manifest_path))
-    if manifest and "depends" in manifest:
-        return manifest.get("depends")
+class Module():
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+        self.package = os.path.basename(os.path.dirname(os.path.dirname(path)))
 
-def parse_code(manifest_path):
-    for path in glob(os.path.join(os.path.dirname(manifest_path), "**/*.py"), recursive=True):
-        print(f"{path=}")
-        data = parse_file(path)
-        for line in data:
-            if "_inherit" in data:
-                print(f"{line=}")
+        self.depends = set()
+        self.inherits = set()
+        self.names = set()
 
+    def parse_deps(self):
+        if not self.depends:
+            manifest = literal_eval(read_file(self.path))
+            if manifest and "depends" in manifest:
+                self.depends = set(manifest.get("depends"))
+        return self.depends
 
+    def parse_code(self):
+        name_regex = re.compile("[\"']([^\"']*)[\"']")
+
+        inherit_check = re.compile("^ *_inherit *= *[\"'].*[\"'] *$")
+        name_check = re.compile("^ *_name *= *[\"'].*[\"'] *$")
+
+        for path in glob(os.path.join(os.path.dirname(self.path), "**/*.py"), recursive=True):
+            data = parse_file(path)
+            for line in data:
+                if re.match(inherit_check, line): #"_inherit =" in line:
+                    if "{" not in line and "[" not in line: # one line inherit
+                        res = re.search(name_regex, line)
+                        if res:
+                            self.inherits.add(res.groups()[0])
+
+                    else:
+                        pass
+                        # TODO: Multiline inheritance
+                if re.match(name_check, line):
+                    res = re.search(name_regex, line)
+                    if res:
+                        self.names.add(res.groups()[0])
+
+    #    if self.names and self.inherits:
+    #        print(f"{self.name} {self.names} {self.inherits}")
+
+    def validate_names(self, modules):
+        for inherit in self.inherits:
+            if not self.validate_name(inherit, modules):
+                print(f"{self.package} {self.name} missing dependency for {inherit}")
+
+    def validate_name(self, name, modules):
+        if name in self.names:
+            return True
+        for dep in self.depends:
+            dep_module = modules.get(dep)
+            if dep_module and dep_module.validate_name(name, modules):
+                return True
+        return False
 
 if __name__ == "__main__":
+    modules = {}
     for name, path in find_modules():
-#        print(f"{name=} {path=} {parse_deps(path)=}")
-        print(f"{name=}")
-        parse_code(path)
+        module = Module(name, path)
+        modules[name] = module
+        module.parse_code()
+        module.parse_deps()
 
+    for module in modules.values():
+        module.validate_names(modules)
