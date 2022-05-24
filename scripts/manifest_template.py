69ab77c1d0cf7f52777e02f18bb1d3bcb49d2db3
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-05-24 13:08:36 +0000

    updates to template

--- scripts/manifest_template.py
+++ scripts/manifest_template.py
@@ -1,6 +1,6 @@
 {
         'author': 'Vertel AB( <[^>]*>)?',  # Matches either "Vertel AB" or "Vertel AB <$ANYTHING>"
-        'website': '.*vertel.se.*',
-        'license': 'AGPL-3',
+        'website': '.*(vertel.se|github.com/vertelab/).*',
+        'license': '(AGPL-3|OPL-1)',
         'description': '.*',  # At least 8 characters
 }
