0504cb1bf473f0553785abb48d1171e68009904d
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-05-24 11:37:47 +0000

    Added check_manifests script to validate manifests

---
+++ scripts/manifest_template.py
@@ -0,0 +1,6 @@
+{
+        'author': 'Vertel AB( <[^>]*>)?',  # Matches either "Vertel AB" or "Vertel AB <$ANYTHING>"
+        'website': '.*vertel.se.*',
+        'license': 'AGPL-3',
+        'description': '.*',  # At least 8 characters
+}
