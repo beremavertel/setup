c0566e5a831bcd2cc4c8e76e698587f0350a9eac
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-06-07 14:07:54 +0000

    Moved scripts to odootools

--- scripts/manifest_template.py
+++
@@ -1,6 +0,0 @@
-{
-        'author': 'Vertel AB( <[^>]*>)?',  # Matches either "Vertel AB" or "Vertel AB <$ANYTHING>"
-        'website': '.*(vertel.se|github.com/vertelab/).*',
-        'license': '(AGPL-3|OPL-1)',
-        'description': '.........*',  # At least 8 characters
-}
